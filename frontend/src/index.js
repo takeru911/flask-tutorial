8d2a0b280f6d901ca6d1cf43af0c09e79e92a53d
takeru911 <t.maehara911@gmail.com>
2020-05-07 21:33:44 +0900

    flaskrのnavbatをreactで

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
 
 
-const MyComponent = () => {
-    const [value, setValue] = useState(null);
+const Navbar = () => {
+    const [userName, setUserName] = useState(null);
 
-    useEffect( () => {
-        async function fetchData() {
-            try {
-                const response = await fetch(`${API_ENDPOINT}/hello`);
-                const responseJson = await response.json();
-                setValue(responseJson.value);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchData();
-    }, [value]);
+    useEffect(() => {
+
+    })
 
     return (
-        <div>
-            value:
-            {!value ? "is Loading": value}
-        </div>
+        <ul>
+            {userName
+                ?
+                <div>
+                    <li><span>{userName}</span></li>
+                    <li><a href="/auth/logout">Log out</a> </li>
+                </div>
+                :
+                <div>
+                    <li><a href="/auth/register">Register</a></li>
+                    <li><a href="/auth/login">Log In</a></li>
+                </div>
+            }
+        </ul>
     )
 }
 
 ReactDOM.render(
     <React.StrictMode>
-        <MyComponent />
+        <Navbar />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('nav-bar')
+);
+
+
+ReactDOM.render(
+    <React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('content')
 );
